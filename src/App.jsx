e4f05f3084adf7f8127f24f6552df8489d9260f3
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-17 21:02:12 -0500

    add Footer, edit ccs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
+import { Footer } from "./Footer";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <Content />
+      <Footer />
       
     </div>
   )
