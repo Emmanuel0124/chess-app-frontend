030fe4200ded24bc25c6ae1bc3bf9646498fe66c
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-17 21:49:36 -0500

    add display for games

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,25 @@
-import { GamesIndex } from "./GamesIndex"
+import axios from "axios";
+import { useState, useEffect } from "react"; 
+
+import { GamesIndex } from "./GamesIndex";
+
 export function Content() {
+  const [games, setGames] = useState([]);
+
+     const handleIndexGames = () => {
+       console.log("handleIndexGames");
+       axios.get("http://localhost:3000/games.json").then((response) => {
+         console.log(response.data);
+         setGames(response.data);
+       });
+     };
+  
+     useEffect(handleIndexGames, []);
+  
+
   return (
     <div>
-     <GamesIndex />
+       <GamesIndex games={games} />
     </div>
-  )
+  );
 }
--- src/GamesIndex.jsx
+++ src/GamesIndex.jsx
@@ -1,7 +1,14 @@
-export function GamesIndex() {
+export function GamesIndex(props) {
   return (
     <div>
       <h1>All games</h1>
+      {props.games.map((game) => (
+         <div key={game.id}>
+           <img src={game.image_url} />
+           <p>result: {game.result}</p>
+           <p>winner: {game.winner}</p>
+         </div>
+       ))}
     </div>
   );
 }
