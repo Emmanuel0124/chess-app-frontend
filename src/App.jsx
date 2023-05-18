75c2568c5ffa05eed490923ada8564425dd74bf2
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-17 20:54:29 -0500

    add content function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Header } from "./Header";
+import { Content } from "./Content";
+
 
 function App() {
   console.log("hello");
   return (
     <div>
       <Header />
+      <Content />
       
     </div>
   )
