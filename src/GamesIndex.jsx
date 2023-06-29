e4a97d0a74b66b402e3bb386feaaf6635d378ee1
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-29 15:39:16 -0500

    add new cards to gamesindex

--- src/GamesIndex.jsx
+++ src/GamesIndex.jsx
@@ -3,14 +3,18 @@ import "./GamesIndex.css";
 export function GamesIndex(props) {
   return (
     <div className="container">
-      <h1>All games</h1>
-      {props.games.map((game) => (
-         <div className="game-item" key={game.id}>
-           <img className="image" src={game.image_url} />
-           <p className="game-result">result: {game.result}</p>
-           <p className="game-winner">winner: {game.winner}</p>
-         </div>
-       ))}
+      <h1 className="title">All games</h1>
+      <div className="card-container">
+        {props.games.map((game) => (
+          <div className="game-card" key={game.id}>
+            <img className="image" src={game.image_url} alt={game.title} />
+            <div className="game-details">
+              <p className="game-result">Result: {game.result}</p>
+              <p className="game-winner">Winner: {game.winner}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
