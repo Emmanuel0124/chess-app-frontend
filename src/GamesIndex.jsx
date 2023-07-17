ebaca8bfe6fafabc0a0d623e5c72c48e38f8c1df
EmmanuelBahena <mannybahena04@gmail.com>
2023-07-17 11:32:39 -0500

    clean up in fromation display on games show

--- src/GamesIndex.jsx
+++ src/GamesIndex.jsx
@@ -10,7 +10,7 @@ export function GamesIndex(props) {
           <div className="game-card" key={game.id}>
             <img className="image" src={game.image_url} alt={game.title} />
             <div className="game-details">
-              <p className="game-winner">Winner: {game.winner} by {game.result}</p>
+              <p className="game-winner">Winner: {game.winner}</p>
               <p className="game-number_of_moves">Number of moves: {game.number_of_moves}</p>
               <Link to={`/games/${game.id}`} className="button">See Game</Link>
             </div>
--- src/GamesShow.jsx
+++ src/GamesShow.jsx
@@ -20,10 +20,7 @@ export function GamesShow() {
       <div className="game-card" key={game.id}>
             <img className="image" src={game.gif_url} alt={game.title} />
             <div className="game-details">
-              <p className="game-number">Number of moves: {game.number_of_moves}</p>
-              <p className="game-result">Result: {game.result}</p>
-              <p className="game-winner">Winner: {game.winner}</p>
-              <p className="game-loser">Loser: {game.loser}</p>
+              <p className="game-winner">{game.winner} won by {game.result}, in {game.number_of_moves} total moves against {game.loser}</p>
               <p className="game-white">White player: {game.white_player}</p>
               <p className="game-black">Black player: {game.black_player}</p>
             </div>
