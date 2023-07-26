12feecb1381b84117663feda00e27aae21941da4
EmmanuelBahena <mannybahena04@gmail.com>
2023-07-26 11:29:05 -0500

    add comment create axios request, edit comment display to have comment box and button

--- src/GamesShow.jsx
+++ src/GamesShow.jsx
@@ -3,17 +3,15 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 export function GamesShow() {
+  const [game, setGame] = useState({});
   const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState(""); // State to hold the new comment input
 
-  const [game, setGame] = useState({})
   const params = useParams();
 
   const getShowGame = () => {
-    console.log(params.id);
-
     axios.get(`http://localhost:3000/games/${params.id}.json`).then(response => {
-      console.log(response.data)
-      setGame(response.data)
+      setGame(response.data);
     });
   };
 
@@ -28,23 +26,49 @@ export function GamesShow() {
     getGameComments();
   }, []);
 
+  const handleCommentChange = (event) => {
+    setNewComment(event.target.value);
+  };
+
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("subbmit")
+
+    // Send the new comment to the backend and update the frontend
+    axios.post(`http://localhost:3000/comments.json`, { content: newComment, game_id: game.id }).then(() => {
+
+      console.log(newComment)
+      // After the comment is successfully created, fetch updated comments
+      getGameComments();
+      // Clear the newComment input after submission
+      setNewComment("");
+    });
+  };
+
   return (
     <div className="container">
       <div className="game-card" key={game.id}>
-            <img className="image" src={game.gif_url} alt={game.title} />
-            <div className="game-details">
-              <p className="game-winner">{game.winner} won by {game.result}, in {game.number_of_moves} total moves against {game.loser}</p>
-              <p className="game-white">White player: {game.white_player}</p>
-              <p className="game-black">Black player: {game.black_player}</p>
-            </div>
-          </div>
-          <div className="card">
-            <div>
-              {comments.map(comment => (
-                <p key={comment.id}>{comment.content} </p>
-              ))}
-            </div>
-          </div>
+        <img className="image" src={game.gif_url} alt={game.title} />
+        <div className="game-details">
+          <p className="game-winner">{game.winner} won by {game.result}, in {game.number_of_moves} total moves against {game.loser}</p>
+          <p className="game-white">White player: {game.white_player}</p>
+          <p className="game-black">Black player: {game.black_player}</p>
+        </div>
+      </div>
+      <div className="card">
+        <div>
+          {comments.map(comment => (
+            <p key={comment.id}>{comment.content}</p>
+          ))}
+        </div>
+        <div>
+          <form onSubmit={handleCommentSubmit}>
+            <input placeholder="Leave a comment!" name="content" type="text" value={newComment} onChange={handleCommentChange}/>
+            <button type="submit">Comment</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
