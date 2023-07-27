0e658068dc2c9cf448957bb702f8767edfee839c
EmmanuelBahena <mannybahena04@gmail.com>
2023-07-26 20:26:53 -0500

    edit game show display

--- src/GamesShow.jsx
+++ src/GamesShow.jsx
@@ -49,19 +49,17 @@ export function GamesShow() {
   return (
     <div className="container">
       <div className="game-card" key={game.id}>
-        <img className="image" src={game.gif_url} alt={game.title} />
         <div className="game-details">
+          <p className="game-players">White player: {game.white_player},  Black player: {game.black_player} </p>
           <p className="game-winner">{game.winner} won by {game.result}, in {game.number_of_moves} total moves against {game.loser}</p>
-          <p className="game-white">White player: {game.white_player}</p>
-          <p className="game-black">Black player: {game.black_player}</p>
         </div>
-      </div>
-      <div className="card">
-        <div>
+        <img className="image" src={game.gif_url} alt={game.title} />
+        <div className="card">
           {comments.map(comment => (
             <p key={comment.id}>{comment.content}</p>
           ))}
         </div>
+        <br/>
         <div>
           <form onSubmit={handleCommentSubmit}>
             <input placeholder="Leave a comment!" name="content" type="text" value={newComment} onChange={handleCommentChange}/>
