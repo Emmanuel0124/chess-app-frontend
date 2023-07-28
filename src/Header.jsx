bbf82954dd34aa0073c911093b201070c830579d
EmmanuelBahena <mannybahena04@gmail.com>
2023-07-27 21:27:51 -0500

    edit header

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,14 +8,14 @@ export function Header() {
     <header className="header-container">
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Check Mates</a>
+    <a className="navbar-brand" href="/">Check Mates</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link" aria-current="page" href="/">Home</a>
-        <a className="nav-link" href="/login">Login</a>
+        <a className="nav-link" aria-current="page" href="/login">Login</a>
+        <a className="nav-link" href="/signup">Signup</a>
         <LogoutLink />
       </div>
     </div>
